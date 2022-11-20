dd152dab9d986b71d1af1f14efb6823178b9ee3a
alucard93 <m.vinicius93.brasilmortal@gmail.com>
2022-11-20 18:54:26 -0300

    update mundo3.py

---
+++ mundo3.py
@@ -0,0 +1,26 @@
+import os 
+
+os.system('cls')
+x = ('arroz', 'feijão', 'queijo')
+# x[0] = 'gelatina' #erro tuplas são imutáveis
+# print(x)
+
+# for comida in x:
+#     print('E vou comer {}'.format(comida))
+
+# for comida in range(0,len(x)):
+#     print('Eu vou comer {}'.format(x[comida]))
+
+# for i, comida in enumerate(x):
+#     print('Eu vou comer {} que está na posição {}'.format(comida, i))
+# print(sorted(x))
+
+# somar tuplas realiza concatenação
+a = (2, 3, 4, 5)
+b = (4, 3, 6, 15)
+c = a + b
+# print(c) 
+# print(c.index(6)) 
+# print(len(c))
+
+
