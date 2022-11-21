ac19e2766a9934587a449471e02a075840d7daf1
alucard93 <m.vinicius93.brasilmortal@gmail.com>
2022-11-20 23:05:38 -0300

    feature ex-formatação

--- mundo3.py
+++ mundo3.py
@@ -31,3 +31,25 @@ a = [2, 3, 4, 5]
 #     print('Tente novamente')
 # print('VocÊ digitou o número {}'.format(nomes[num]))
 
+# print(max(a))
+
+listagem = (
+    'Lápis', 1.75,
+    'Borracha', 2,
+    'Caderno', 15.90,
+    'Estojo', 25,
+    'Transferidor', 4.20,
+    'Compasso', 9.99,
+    'Mochila', 120.32,
+    'Canetas', 22.30,
+    'Livro', 34.90
+)
+# print('-' * 39)
+# print(f'{"LISTAGEM DE PRECOS":^39}')
+# print('-' * 39)
+# for pos in range(0, len(listagem)):
+#     if pos % 2 == 0:
+#         print(f'{listagem[pos]:.<30}', end ='')
+#     else:
+#         print(f'R${listagem[pos]:>7.2f}')
+# print('-' * 39)
